5027a52dae65bbda5075325ad95108bcaebfa7a4
RalonPlayz <79267114+RalonPlayz@users.noreply.github.com>
2021-05-27 16:28:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+Webcam.set({
+    width: 310,
+    height:300,
+    image_format:'png',
+    png_quality:90,
+
+    constraints:{
+        facingmode:"Environment"
+    }
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
